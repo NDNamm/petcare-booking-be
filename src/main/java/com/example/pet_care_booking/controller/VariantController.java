9254173b721ed4f4bfd0a132a7bcb4ce3ee59902
Nguyễn Quang Anh <nguyenquanganh07042004@gmail.com>
2025-08-29 13:54:34 +0700

    update

---
+++ src/main/java/com/example/pet_care_booking/controller/VariantController.java
@@ -0,0 +1,28 @@
+package com.example.pet_care_booking.controller;
+
+import com.example.pet_care_booking.service.VariantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/variant/")
+public class VariantController {
+    @Autowired
+    private VariantService variantService;
+
+//    @PutMapping("/edit/:id")
+//    private ResponseEntity<Variants> updateVariant(@PathVariable Long id, @RequestBody VariantDTO variantDTO) {
+//        return ResponseEntity.ok(variantService.updateVariant(id, variantDTO));
+//    }
+
+    @DeleteMapping("/delete/:id")
+    private ResponseEntity<Void> updateVariant(@PathVariable Long id) {
+        variantService.deleteVariant(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
